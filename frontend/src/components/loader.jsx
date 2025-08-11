a063ce23bb5556374312b7634937848549b39781
Priyaranjan <cse.priyaranjan@gmail.com>
2025-08-11 09:47:42 +0530

    Update loader.jsx

--- frontend/src/components/loader.jsx
+++ frontend/src/components/loader.jsx
@@ -1,9 +1,8 @@
-// components/Loader.jsx
 import React from "react";
 
 const Loader = () => {
   return (
-    <div className="fixed inset-0 bg-black bg-opacity-30 flex items-center justify-center z-50">
+    <div className="fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm flex items-center justify-center z-50">
       <div className="w-12 h-12 border-4 border-white border-t-transparent rounded-full animate-spin"></div>
     </div>
   );
